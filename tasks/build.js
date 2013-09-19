063c80e5b8976ff2f5a3692ff07cd63ca8227509
clement <maboiteaspam@wanadoo.fr>
2013-09-19 19:52:51 +0800

    commit

--- tasks/build.js
+++ tasks/build.js
@@ -5,11 +5,11 @@ module.exports = function(grunt) {
     grunt.registerTask("loader_builder", "Builds a loader builder script jit", function(task_file, task, args_n, args_nn, args_nnn){
         var done = this.async();
         var options = grunt.file.readJSON(task_file);
-        grunt.config.init(options)
-        grunt.config.set(task, options[task])
-        var build_cmd = this.nameArgs
-        build_cmd = build_cmd.substring("loader_builder:".length)
-        build_cmd = build_cmd.substring((task_file+":").length)
+        grunt.config.init(options);
+        grunt.config.set(task, options[task]);
+        var build_cmd = this.nameArgs;
+        build_cmd = build_cmd.substring("loader_builder:".length);
+        build_cmd = build_cmd.substring((task_file+":").length);
         grunt.task.run(build_cmd);
         done();
     });
@@ -19,34 +19,35 @@ module.exports = function(grunt) {
             "copy":{}
             ,"meta_merge":{}
         });
-        grunt.verbose.writeflags(options, 'Options')
-        var copy = options.copy
-        var meta_merge = options.meta_merge
+        grunt.verbose.writeflags(options, 'Options');
+        var copy = options.copy;
+        var meta_dir = options.meta_dir;
+        var meta_merge = options.meta_merge;
 
         for(var out_file in copy ){
-            var in_file = copy[out_file]
-            grunt.file.copy(in_file, out_file)
-            grunt.log.ok("Copied to "+out_file)
+            var in_file = copy[out_file];
+            grunt.file.copy(in_file, out_file);
+            grunt.log.ok("Copied to "+out_file);
         }
 
         for(var out_file in meta_merge ){
-            var in_files = meta_merge[out_file]
+            var in_files = meta_merge[out_file];
             var meta_merged = {
                 "build":[]
                 ,"dependences":[]
             }
             for( var n in in_files ){
-                var meta_obj = grunt.file.readJSON(in_files[n])
+                var meta_obj = grunt.file.readJSON(meta_dir+""+in_files[n]);
 
                 for( var t in meta_obj["build"] ){
-                    meta_merged["build"].push( meta_obj["build"][t] )
+                    meta_merged["build"].push( meta_obj["build"][t] );
                 }
                 for( var t in meta_obj["dependences"] ){
-                    meta_merged["dependences"].push( meta_obj["dependences"][t] )
+                    meta_merged["dependences"].push( meta_obj["dependences"][t] );
                 }
             }
-            grunt.file.write(out_file, JSON.stringify(meta_merged, null, 4))
-            grunt.log.ok("Merged to "+out_file)
+            grunt.file.write(meta_dir+out_file, JSON.stringify(meta_merged, null, 4));
+            grunt.log.ok("Merged to "+out_file);
         }
 
         grunt.log.ok()
